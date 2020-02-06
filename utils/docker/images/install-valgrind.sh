dceec0a395781097ae29543d7886da87a7317948
Raoni Fassina Firmino <raoni@linux.ibm.com>
2020-02-06 18:29:04 -0300

    common: change valgrind version for ppc64le docker image
    pmem/valgrind don't support ppc64le yet, so we keep using the distro
    supplied one so at least we can run the tests that use valgrind
    without pmemcheck tool.
    
    Signed-off-by: Raoni Fassina Firmino <raoni@linux.ibm.com>

--- utils/docker/images/install-valgrind.sh
+++ utils/docker/images/install-valgrind.sh
@@ -8,13 +8,31 @@
 
 set -e
 
-git clone https://github.com/pmem/valgrind.git
-cd valgrind
-# valgrind v3.15 with pmemcheck
-git checkout c27a8a2f973414934e63f1e94bc84c0a580e3840
-./autogen.sh
-./configure
-make -j$(nproc)
-make -j$(nproc) install
-cd ..
-rm -rf valgrind
+install_upstream_from_distro() {
+  if command -v dnf &>/dev/null ; then
+    dnf install -y valgrind
+  elif command -v apt-get &>/dev/null ; then
+    apt-get install -y --no-install-recommends valgrind
+  else
+    return 1
+  fi
+}
+
+install_custom-pmem_from_source() {
+  git clone https://github.com/pmem/valgrind.git
+  cd valgrind
+  # valgrind v3.15 with pmemcheck
+  git checkout c27a8a2f973414934e63f1e94bc84c0a580e3840
+  ./autogen.sh
+  ./configure
+  make -j$(nproc)
+  make -j$(nproc) install
+  cd ..
+  rm -rf valgrind
+}
+
+ARCH=$(uname -m)
+case $ARCH in
+  ppc64le) install_upstream_from_distro ;;
+  *) install_custom-pmem_from_source ;;
+esac
